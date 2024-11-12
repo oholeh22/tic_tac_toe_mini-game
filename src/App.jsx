10d7798190e6b5cdce46b390cbfbde3ca0743e95
Oleh Shyian <shiyan4242@gmail.com>
2024-11-12 19:26:20 +0100

    tic-toc-toe

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+}
+
+render() {
+  return (
+    <div className="tic-tac-toe">
+
+    </div>
+  )
+}
+
+export default App;
